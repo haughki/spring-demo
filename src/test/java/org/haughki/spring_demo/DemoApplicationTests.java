54506bbb662c2557e44239e2792b8d660ae41259
Hawkeye Parker <parker.hawkeye@gmail.com>
2021-01-24 11:50:43 -0800

    first commit -- project skeleton

---
+++ src/test/java/org/haughki/spring_demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.haughki.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
